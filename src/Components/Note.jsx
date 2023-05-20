1d209c06dd6d598e0d11eed51c25e2e09565c017
harmeet9013 <evildoremon@gmail.com>
2023-05-20 19:53:12 +0530

    Bug fix for data import

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,26 +1,27 @@
 import "./components.css";
+import React from "react";
 import NoteJS from "./notes";
 import CreateNote from "./CreateNote";
 
 function Note() {
-    // return NoteJS.map((inputs) => (
-    //     <CreateNote Title={inputs.title} Content={inputs.content} />
-    // ));
+    return NoteJS.map((inputs) => (
+        <CreateNote Title={inputs.title} Content={inputs.content} />
+    ));
 
-    const [inputText, setInputText] = useState("");
-    return (
-        <div>
-            <label>Note Title
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    value={inputText}
-                    className=".TitleField"
-                />
-            </label>
-            <p>Your input: {inputText}</p>
-        </div>
-    );
+    // const [inputText, setInputText] = useState("");
+    // return (
+    //     <div>
+    //         <label>Note Title
+    //             <input
+    //                 type="text"
+    //                 onChange={handleChange}
+    //                 value={inputText}
+    //                 className=".TitleField"
+    //             />
+    //         </label>
+    //         <p>Your input: {inputText}</p>
+    //     </div>
+    // );
 }
 
 export default Note;
