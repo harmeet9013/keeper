0e2c17abb7af0fa2568804a073b33fecd987a609
Harmeet Singh <evildoremon@gmail.com>
2023-06-11 20:43:08 +0530

    description and credits added.

--- src/Components/CheckFirstLaunch.jsx
+++ src/Components/CheckFirstLaunch.jsx
@@ -1,6 +1,17 @@
 function CheckLaunchStatus({ firstLaunch }) {
     if (firstLaunch === true) {
-        return <h1 style={{ color: "black" }}>Create some notes!</h1>;
+        return (
+            <h1
+                style={{
+                    color: "black",
+                    marginTop: "-20px",
+                    fontSize: "40px",
+                    fontStyle: "italic",
+                }}
+            >
+                Create some notes!
+            </h1>
+        );
     }
     return null;
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,6 +4,14 @@ function Footer() {
     let CURRENTYEAR = new Date().getFullYear();
     return (
         <footer>
+            <p className="desc-text">
+                This website is a keeper app, a note-taking application that
+                allows users to create, edit, and delete notes. It uses ReactJS
+                to create the user interface and the functionality of the app.
+                ReactJS is a popular JavaScript library for creating user
+                interfaces that are fast and dynamic. The website has a simple
+                and elegant design.
+            </p>
             <p className="Footer">Copyright &#169; {CURRENTYEAR}</p>
         </footer>
     );
